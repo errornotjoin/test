f1322088cfa89f4a460821bc5a73aa97957154b5
ErrorNotJoin <86501620+errornotjoin@users.noreply.github.com>
2023-12-16 12:10:37 +0000

    starting making the div maker (html, css and js) been made

---
+++ projects/projects_Here/the_div_maker/div_maker_js.js
@@ -0,0 +1,24 @@
+//this is where all the div all goto 
+var Div_maker = document.getElementById("Div_maker")
+
+//this will hold all the settings/inputs from the user 
+function Update_all_div_settinge_from_user_input()
+{
+    //div one user input settinges 
+        //this has the updated hight and with data from the user
+        var Div_one_hight_and_width_update_setting = "width:" + document.getElementById(Main_width_1) + "hight: " + document.getElementById(Main_width_1)
+        //this has all the colour 
+        var Div_one_background_colour = "backgeound-color:" + document.getElementById(Main_colour_1)
+    
+    //div two user input settinges 
+        //this has the updated hight and with data from the user
+        var Div_two_hight_and_width_update_setting = "width:" + document.getElementById(Main_width_1) + "hight: " + document.getElementById(Main_width_1)
+        //this has all the colour 
+        var Div_two_background_colour = "backgeound-color:" + document.getElementById(Main_colour_1) 
+
+    //div three user input settinges 
+        //this has the updated hight and with data from the user
+        var Div_three_hight_and_width_update_setting = "width:" + document.getElementById(Main_width_1) + "hight: " + document.getElementById(Main_width_1)
+        //this has all the colour 
+        var Div_three_background_colour = "backgeound-color:" + document.getElementById(Main_colour_1) 
+}
