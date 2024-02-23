64a1e8b7f602fb47a5fd0839756d295a61a8cd2d
ErrorNotJoin <86501620+errornotjoin@users.noreply.github.com>
2024-02-23 18:17:05 +0000

    adding new files and start projects

---
+++ projects/timer/js/Timer.js
@@ -0,0 +1,17 @@
+//getting the main items 
+var Input_1 = document.getElementById("input_1")
+
+var Input_2 = document.getElementById("Input_2")
+
+var Input_3 = document.getElementById("Input_3")
+var the_Start_button = document.getElementById("The_start_button")
+//
+
+//getting the Pre made Items 
+
+
+// all the Bool Items Will be here (only Goble bool)
+
+
+
+
